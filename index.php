f4339c5021a5301d24bea30f11aff00ea4f9dfef
Megan Burnett <m.burnett@accenture.com>
2018-05-04 22:49:49 +0100

    website version0.1

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Creative - Start Bootstrap Theme</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/creative.min.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">IQRA</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="about.php">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="gallery.php">Gallery</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="services.php">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="contact.php">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <header class="masthead"> 
+        <div class="row">
+        <div style="margin-top:10%; margin-left:10%; margin-right:10%;">      
+          <img src="/img/banner.png" class="img-fluid" alt="Responsive image">
+          </div>
+        </div>
+    </header>
+
+    <div class="row" style="margin-top:2%; margin-left:1%; margin-right:1%">
+        <div class="col-md-7">
+        <img src="/img/about-iqra.png" class="img-fluid" alt="Responsive image">
+        </div>
+        <div class="col-md-5"> 
+         <img src="/img/portfolio.png" class="img-fluid" alt="Responsive image">
+        </div>
+    </div>
+    
+      <div class="row" style="margin-top:2%; margin-left:1%; margin-right:1%">
+        <div class="col-md-4">
+                   <img src="/img/service.png" class="img-fluid" alt="Responsive image">
+        </div>
+        <div class="col-md-4">
+                   <img src="/img/blogblock.png" class="img-fluid" alt="Responsive image">
+        </div>
+        <div class="col-md-4">
+                   <img src="/img/contactblock.png" class="img-fluid" alt="Responsive image">
+        </div>
+    </div>
+    
+    <div class="col-lg-8 mx-auto">
+        <p class="text-dark mb-5 text-center">tester</p>
+    </div>
+    
+   <header class="masthead" style="height:10px"> 
+        <div class="row">
+        </div>
+    </header>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/creative.min.js"></script>
+
+  </body>
+
+</html>
