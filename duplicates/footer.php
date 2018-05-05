3992b7dbe2d8772c013dbd55bda7292f1106eaf1
Megan Burnett <m.burnett@accenture.com>
2018-05-05 10:03:44 +0100

    footer php and css

--- duplicates/footer.php
+++ duplicates/footer.php
@@ -1,3 +1,18 @@
-<?php
-echo "tester";
-?>
+<footer class="grey" style="margin-top:2%"> 
+        <div class="row">
+        <div class="container">
+    <hr>
+        <div class="text-center center-block">
+                <a href="https://www.facebook.com/bootsnipp"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
+	            <a href="https://twitter.com/bootsnipp"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
+	            <a href="https://plus.google.com/+Bootsnipp-page"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
+	            <a href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
+</div>
+    <hr>
+</div>
+
+        </div>
+        <p class="text-dark mb-5 text-center" style="font-size:20px;">
+          &copy; Iqra 2018
+</p>
+</footer>
